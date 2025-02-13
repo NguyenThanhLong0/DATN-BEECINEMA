5e601492e101d7c502a0a3a5e7f87b342755fefd
LongDino <longntph45139@fpt.edu.vn>
2025-02-13 23:15:35 +0700

    fix seeder combo cua Hoan sai

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
-use App\Models\ComboFood;
+
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -31,7 +31,7 @@ class DatabaseSeeder extends Seeder
             SeatSeeder::class,
             FoodSeeder::class,
             ComboSeeder::class,
-            ComboFood::class,
+            ComboFoodSeeder::class,
             MoviesSeeder::class,
             MovieVersionSeeder::class,
         ]);
